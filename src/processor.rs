217d9c814b40b621d6c1080833f34ea9988f77a2
Giorgi Chokoshvili <giochoko2006@gmail.com>
2026-06-28 03:09:04 +0400

    Clean up project

--- src/processor.rs
+++
@@ -1,7 +0,0 @@
-use dasp::Frame;
-
-
-pub trait AudioProcessor {
-    fn process<F>(&self, input: &mut [F]) where F:Frame<Sample = f64>; //effect (distortion -> distortion)
-    fn name(&self) -> &str; //returns the name of the pedal
-}
