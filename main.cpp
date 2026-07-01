7ff50592b19c582b94fa1a26ea8ec8867b35c505
ronioz <roniosipov7@gmail.com>
2026-07-01 16:55:48 +0400

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <AudioToolbox/AudioToolbox.h>
-#include <span>
-
-static const float SAMPLE_RATE = 44100;
-static const float BUFFER_SIZE = 32;
+#include <iostream>
+#include "properties.hpp"
+#include "PedalBoard.hpp"
 
 int main() {
-    
+    AudioComponentDescription desc = {};
+    desc.componentType = kAudioUnitType_Output;
+    desc.componentSubType = kAudioUnitSubType_DefaultOutput;
+    desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+
+    AudioComponent comp = AudioComponentFindNext(nullptr, &desc);
+    if(!comp) {
+        std::cerr << "Could not find default output unit.\n";
+        return -1;
+    }
+
+    AudioUnit audioUnit;
+    if(AudioComponentInstanceNew(comp, &audioUnit) != noErr) {
+        std::cerr << "Could not create Audio Unit Instance!\n";
+        return -1;
+    }
+
+    AudioUnitInitialize(audioUnit);
+
+    PedalBoard tempPedalBoard;
+    tempPedalBoard.setupAudioUnit(audioUnit);
+
+    AudioOutputUnitStart(audioUnit);
+
+    std::cout << "Press [ENTER] to stop playback and exit...\n";
+    std::cin.get();
+
+    AudioOutputUnitStop(audioUnit);
+    AudioUnitUninitialize(audioUnit);
+    AudioComponentInstanceDispose(audioUnit);
+
+    return 0;
 }
